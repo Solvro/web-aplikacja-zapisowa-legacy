53a4bbaa132339343e82b5eedc308d563d526c57
Sernikjamnika <mati.walczak997@gmail.com>
2018-08-20 21:16:24 +0200

    Removed files form __pycache__

--- backend/aplikacja_zapisowa/enrolmentpanel/models.py
+++ backend/aplikacja_zapisowa/enrolmentpanel/models.py
@@ -1,6 +1,6 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser, User
+from django.contrib.auth.models import AbstractUser
 from django.core.mail import send_mail
+from django.db import models
 
 
 # NOTE consider using indexes in models
@@ -23,18 +23,40 @@ class Student(AbstractUser):
     # but one student can be registered for several
     # also enables deletion with the rally
     rally = models.ForeignKey(Rally, on_delete=models.CASCADE)
-    # ask if it has to be unique think about login
-    index = models.CharField(max_length=30, unique=True)
+    index = models.CharField(max_length=30)
     sex = models.CharField(max_length=1,
                            choices=SEX_CHOICES)
     room = models.ForeignKey(Room, null=True, on_delete=models.SET_NULL)
 
     def save(self, *args, **kwargs):
-        password = Student.objects.make_random_password()
+        self.username = self.generate_login()
         # add index to ensure being unique
-        self.set_password(raw_password=self.index + password)
+        self.set_password(raw_password=self.generate_pass())
         # send_mail(f"Here is your password {kwargs['index'] + password}",
         #           'mati.walczak997@gmail.com',
         #           [kwargs['email']],
         #           fail_silently=False)
         super().save(*args, **kwargs)
+
+    def generate_login(self):
+        """
+        Generates login from index and
+        address of Rally object in memory
+        :return:
+        """
+        # can be annoying
+        # NOTE think about other way
+        username = self.index + Student.objects.make_random_password()
+        print(f'Username: {username}', end=' ')
+        return username
+
+    def generate_pass(self):
+        """
+        Generates password from index and random 10 sign
+        from [2-9a-hjk-n-p-zA-HJ-Z]
+        :return:
+        """
+        # potentially vulnerable
+        password = self.index + Student.objects.make_random_password()
+        print(f'Pass: {password}')
+        return password
