5c5301ae4aa4550a7cfea164bfe749d0d3b6b010
Michał Treter <adimus13@gmail.com>
2018-08-21 21:27:46 +0200

    Webpack now starts, but serves nothing

--- frontend/webpackDevServer.config.js
+++ frontend/webpackDevServer.config.js
@@ -9,6 +9,7 @@ const NODE_ENV = process.env.NODE_ENV || "development";
 
 const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
 const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || '3000';
 
 module.exports = function(proxy, allowedHost) {
   return {
@@ -74,6 +75,7 @@ module.exports = function(proxy, allowedHost) {
     // Enable HTTPS if the HTTPS environment variable is set to 'true'
     https: protocol === 'https',
     host: host,
+    port: port,
     overlay: false,
     historyApiFallback: {
       // Paths with dots should still use the history fallback.
