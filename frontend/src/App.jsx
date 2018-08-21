d9902dbffe1041d9b73c09018d0e8acf24729234
Maciej Dziadyk <dziadykkm@gmail.com>
2018-08-22 00:32:07 +0200

    Added Reducers, actions, connect redux with redux, enable debbuging via chrome addon

---
+++ frontend/src/App.jsx
@@ -0,0 +1,54 @@
+import Button from '@material-ui/core/Button';
+import { bindActionCreators } from 'redux';
+import * as React from 'react';
+import { connect } from 'react-redux';
+import * as actionCreators from './store/actions/actionsCreators';
+import './App.css';
+
+import logo from './logo.svg';
+
+
+
+function mapStateToProps(state){
+  // tslint:disable-next-line:no-console
+  console.log(state);
+  return {
+    index: state.numbers
+  }
+}
+
+function mapDispachtToProps(dispatch){
+    // tslint:disable-next-line:no-console
+    console.log(dispatch);
+  return bindActionCreators(actionCreators, dispatch);
+
+}
+
+class App extends React.Component {
+
+  add = () => {
+    return true;
+  }
+
+  render() {
+    // tslint:disable-next-line:no-console
+    console.log(this.props);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+          
+        </p>
+        <Button variant="contained" color="primary" onClick={this.add}>
+          {this.props.index}
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps,mapDispachtToProps)(App);
