65db7174561407f8c14b6d4e1784d85e6a46af2f
Maciej Dziadyk <dziadykkm@gmail.com>
2018-08-26 21:15:31 +0200

    router added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,17 @@
 import './App.css';
 
 import Button from '@material-ui/core/Button';
-import * as React from 'react';
-import { MouseEvent } from 'react';
-import { connect, Dispatch } from 'react-redux';
+import React, {MouseEvent} from 'react';
+import {connect, Dispatch} from 'react-redux';
+import {RouteComponentProps} from "react-router-dom";
+import {ApplicationState} from './store';
+import {increment} from './store/increment/actions';
 
-import { ApplicationState } from './store';
-import { increment } from './store/increment/actions';
-
-interface IPropsFromStore {
+interface IPropsFromStore extends RouteComponentProps<any>{
     store: ApplicationState;
     add(value: number): void;
 }
+
 interface IAppState {
   state: ''
 }
@@ -28,7 +28,7 @@ class App extends React.Component<IPropsFromStore, IAppState> {
   public render() {
     // tslint:disable-next-line:no-console
     console.log('props',this.props);
-    return (
+      return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
@@ -40,12 +40,16 @@ class App extends React.Component<IPropsFromStore, IAppState> {
         <Button variant="contained" color="primary" onClick={this.onClickAdd}>
           Add 10 to store
         </Button>
+        <Button variant="contained" color="primary" onClick={this.onClickChange} >
+            Go to The best site you have ever seen!
+        </Button>
         <p>Store: {`${this.props.store.incrementStore.number}`}</p>
       </div>
     );
   }
 
     private onClickAdd = (event: MouseEvent<HTMLElement>) => this.props.add(10);
+    private onClickChange = (event: MouseEvent<HTMLElement>) => this.props.history.push('/a');
 }
-
+// this.props.history.push("/a")
 export default connect(mapStateToProps,mapDispatchToProps)(App);
---
+++ frontend/src/components/LoginScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class LoginScreen extends React.Component{
+    public render() {
+        // tslint:disable-next-line:no-console
+        console.log('props',this.props);
+        return <div> Jest Super! </div>
+    }
+}
+
+export default LoginScreen;
