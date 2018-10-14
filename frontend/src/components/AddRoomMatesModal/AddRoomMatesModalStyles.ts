501cb265e8b02bd162711adbcb44a8a9128088d8
Maciej Dziadyk <maciejdziadyk@Maciejs-MacBook-Pro.local>
2018-10-15 00:44:58 +0200

    AddRoomMatesModal done

--- frontend/src/components/AddRoomMatesModal/AddRoomMatesModalStyles.ts
+++ frontend/src/components/AddRoomMatesModal/AddRoomMatesModalStyles.ts
@@ -7,22 +7,42 @@ export const addRoomMatesModalStyles = (theme: Theme) => createStyles({
         alignItems: 'center',
     },
     paperContainer: {
+        display: 'flex',
         flexDirection: 'column',
         padding: '80px 0'
     },
     paper: {
         minHeight: '50vh',
+        padding: '2em',
         flexDirection: 'column',
-        padding: '1em',
     },
     buttonWrapper: {
         display: 'flex',
         flexDirection: 'row',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: '2em 0'
     },
     button: {
         marginRight: '12px'
     },
     textfield: {
-        color: '#000'
+        color: 'black'
+    },
+    description: {
+        textAlign: 'justify',
+        fontSize: '24px',
+        fontWeight: 'normal'
+    },
+    chipsContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        margin: '0 auto'
+
+    },
+    chip:{
+        marginBottom: '10px'
     }
 });
