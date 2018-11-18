a29f9e8b115605b6b263dd42ddfd537447a727b1
Michał Treter <adimus@Mac-mini-Micha.local>
2018-11-18 22:16:09 +0100

    Richer token response

--- backend/aplikacja_zapisowa/enrolmentpanel/urls.py
+++ backend/aplikacja_zapisowa/enrolmentpanel/urls.py
@@ -7,24 +7,27 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView
 )
 
-from .views import StudentView
+from enrolmentpanel.views import (
+    CustomTokenObtainPairView,
+    StudentView
+)
 
 
 urlpatterns = [
     path('auth/', include('rest_framework.urls')),
     path('organiser/', include('enrolmentpanel.organiser.urls')),
     path('student/', include('enrolmentpanel.student.urls')),
-    path('token', TokenObtainPairView.as_view()),
+    path('token', CustomTokenObtainPairView.as_view()),
     path('token/refresh', TokenRefreshView.as_view()),
-    path('token/', TokenObtainPairView.as_view()),
+    path('token/', CustomTokenObtainPairView.as_view()),
     path('token/refresh/', TokenRefreshView.as_view()),
-    path('students/<username>', StudentView.as_view())
+    path('token/verify', TokenVerifyView.as_view()),
+    path('students/<username>', StudentView.as_view()),
 ]
 
 if settings.DEBUG:
     urlpatterns.extend(
         [
             path('test/', include('enrolmentpanel.test.urls')),
-            path('token/verify', TokenVerifyView.as_view()),
         ]
     )
--- backend/aplikacja_zapisowa/enrolmentpanel/views.py
+++ backend/aplikacja_zapisowa/enrolmentpanel/views.py
@@ -1,11 +1,59 @@
 from django.conf import settings
 from django.shortcuts import get_object_or_404
+
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 from enrolmentpanel.models import Student, User
 from enrolmentpanel.serializers import StudentSerializer
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super(CustomTokenObtainPairSerializer, cls).get_token(user)
+
+        if user.is_participant:
+            logged_student = Student.objects.get(user=user)
+            serializer = StudentSerializer(logged_student)
+
+            token['student'] = serializer.data
+            token['room'] = (not logged_student.room is None)
+            serialized_room_mates = []
+
+            if token['room']:
+                room_mates = Student.objects.get(room=logged_student.room)
+                
+                try:
+                    for room_mate in room_mates:
+                        if room_mate != logged_student:
+                            serialized_room_mates.append(
+                                StudentSerializer(room_mate).data
+                            )
+                except Exception:
+                    pass
+
+            token['roomMates'] = serialized_room_mates
+
+        return token
+
+    def validate(self, attrs):
+        data = super(CustomTokenObtainPairSerializer, self).validate(attrs)
+
+        token = self.get_token(self.user)
+        data['student'] = token['student']
+        data['room'] = token['room']
+        data['roomMates'] = token['roomMates']
+
+        return data
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
 
 
 class StudentView(APIView):
