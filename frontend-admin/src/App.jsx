b01ea3228a46eae4297aa097f2dd871181974e4e
Jan Librowski <janlibrowski@gmail.com>
2019-02-18 15:22:54 +0200

    add css baseline

---
+++ frontend-admin/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+function App() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+    </React.Fragment>
+  );
+}
+
+export default App;
