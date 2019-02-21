2daf747e50039a592d829fb67c3969750643dc77
Adrian Mucha <kestarumper8@gmail.com>
2019-02-21 16:27:08 +0100

    add Tile component with fixed Theme CSS

--- frontend-admin/src/routes/Trip/OverviewRoute.jsx
+++ frontend-admin/src/routes/Trip/OverviewRoute.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import {withRouter} from "react-router-dom";
+import Tile from "../../components/Tile"
+import DescriptionIcon from "@material-ui/icons/Description"
+import { Button } from '@material-ui/core';
 
 class OverviewRoute extends Component {
   constructor(props) {
@@ -7,13 +10,15 @@ class OverviewRoute extends Component {
     this.state = {};
   }
 
-
   render() {
     const { match } = this.props;
     return (
       <div>
         Overview
         { match.params.id }
+        <Tile title={"Informacje"} icon={<DescriptionIcon color="primary"/>}>
+            <Button color="primary">Klik</Button>
+        </Tile>
       </div>
     );
   }
