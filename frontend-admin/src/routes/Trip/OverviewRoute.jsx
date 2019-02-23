cd0362e4eed7cd3c0c25b7c5826a0bfc111da898
Adrian Mucha <kestarumper8@gmail.com>
2019-02-23 17:14:05 +0100

    add gridlist

--- frontend-admin/src/routes/Trip/OverviewRoute.jsx
+++ frontend-admin/src/routes/Trip/OverviewRoute.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import {withRouter} from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import Tile from "../../components/Tile"
 import DescriptionIcon from "@material-ui/icons/Description"
-import { Button } from '@material-ui/core';
+import { Button, GridList, GridListTile } from '@material-ui/core';
 import DashboardHeader from '../../components/DashboardHeader';
 
 class OverviewRoute extends Component {
@@ -15,16 +15,21 @@ class OverviewRoute extends Component {
     const { match } = this.props;
     return (
       <div>
-        Overview
-        { match.params.id }
-        <DashboardHeader 
-        title='Jesienny Rajd Mechanika'
-        subtitle='Wieloletnia tradycja wydziału mechanicznego, głuchołazy bla bla'
-        date={{day: 'Piątek', full: '31 marca'}}
-        />
-        <Tile title={"Informacje"} icon={<DescriptionIcon color="primary"/>}>
-            <Button color="secondary">Klik</Button>
-        </Tile>
+        <GridList cellHeight={180} cols={3} spacing={16}>
+          <GridListTile key="Subheader" cols={3} style={{ height: 'auto' }}>
+            <DashboardHeader
+              title='Jesienny Rajd Mechanika'
+              subtitle='Wieloletnia tradycja wydziału mechanicznego, głuchołazy bla bla'
+              date={{ day: 'Piątek', full: '31 marca' }}
+            />
+          </GridListTile>
+
+          <GridListTile>
+            <Tile title={"Informacje"} icon={<DescriptionIcon color="primary" />}>
+              <Button color="secondary">Klik</Button>
+            </Tile>
+          </GridListTile>
+        </GridList>
       </div>
     );
   }
