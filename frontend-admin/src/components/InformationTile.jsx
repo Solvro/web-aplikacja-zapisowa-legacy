8d0197c37998f352a33642b101e48c02b11f5166
Adrian Mucha <kestarumper8@gmail.com>
2019-02-24 18:42:46 +0100

    change information icons

--- frontend-admin/src/components/InformationTile.jsx
+++ frontend-admin/src/components/InformationTile.jsx
@@ -4,13 +4,15 @@ import Tile from './Tile'
 import ContentListItem from './ContentListItem'
 import { List } from '@material-ui/core';
 import CalendarIcon from '@material-ui/icons/DateRange'
+import LocationIcon from '@material-ui/icons/LocationOn'
+import BedIcon from '@material-ui/icons/LocalHotel'
 
 function InformationTile(props) {
     return <Tile title={"Informacje"} icon={<DescriptionIcon color="primary" />}>
         <List>
             <ContentListItem title={'Termin wycieczki'} value={'Kappa'} icon={CalendarIcon} />
-            <ContentListItem title={'Termin wycieczki'} value={'Kappa'} icon={CalendarIcon} />
-            <ContentListItem title={'Termin wycieczki'} value={'Kappa'} icon={CalendarIcon} />
+            <ContentListItem title={'Termin wycieczki'} value={'Kappa'} icon={LocationIcon} />
+            <ContentListItem title={'Termin wycieczki'} value={'Kappa'} icon={BedIcon} />
         </List>
     </Tile>
 }
