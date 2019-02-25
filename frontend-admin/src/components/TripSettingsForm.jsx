5c1b04c114d8ac55ced0960d3362debd17ee37d6
Adrian Mucha <kestarumper8@gmail.com>
2019-02-25 23:57:26 +0100

    finish good look

--- frontend-admin/src/components/TripSettingsForm.jsx
+++ frontend-admin/src/components/TripSettingsForm.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { withStyles, Grid } from '@material-ui/core';
+import { withStyles, Grid, Button } from '@material-ui/core';
 import DateIcon from "@material-ui/icons/DateRange";
 import HotelIcon from "@material-ui/icons/LocalHotel";
 import LocationIcon from "@material-ui/icons/LocationOn";
 import DescriptionIcon from "@material-ui/icons/Description";
 import FormTextInput from './FormTextInput';
 import UploadFileInput from './UploadFileInput';
+import { DatePicker, MuiPickersUtilsProvider } from 'material-ui-pickers';
+import MomentUtils from '@date-io/moment'
 
 const styles = theme => ({
     root: {
@@ -13,11 +15,31 @@ const styles = theme => ({
         marginLeft: 'auto',
         marginRight: 'auto',
         padding: theme.spacing.unit * 2,
-        border: '1px solid black'
     },
+
+    button: {
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        margin: theme.spacing.unit * 2,
+        width: '75%',
+        textAlign: 'center'
+    }
 });
 
 class TripSettingsForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            dateStart: new Date(),
+            dateEnd: new Date()
+        }
+    }
+
+    handleDateChange = name => date => {
+        this.setState({
+            [name]: date,
+        });
+    };
 
     render() {
         const { classes } = this.props
@@ -35,12 +57,43 @@ class TripSettingsForm extends React.Component {
                     spacing={24}
                 >
                     <Grid item xs={12} sm={6}>
-                        <UploadFileInput onChange={console.log} label={'Uczestnicy'}/>
+                        <UploadFileInput onChange={console.log} label={'Uczestnicy'} />
                     </Grid>
                     <Grid item xs={12} sm={6}>
-                        <UploadFileInput label={'Zdjęcie tła'}/>
+                        <UploadFileInput label={'Zdjęcie tła'} />
                     </Grid>
                 </Grid>
+                <MuiPickersUtilsProvider utils={MomentUtils}>
+                    <Grid
+                        container
+                        direction="row"
+                        justify="space-between"
+                        alignItems="center"
+                        spacing={24}
+                    >
+                        <Grid item xs={12} sm={6}>
+                            <DatePicker
+                                fullWidth
+                                margin="normal"
+                                label="Date picker"
+                                onChange={this.handleDateChange('dateStart')}
+                                value={this.state.dateStart}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <DatePicker
+                                fullWidth
+                                margin="normal"
+                                label="Date picker"
+                                onChange={this.handleDateChange('dateEnd')}
+                                value={this.state.dateEnd}
+                            />
+                        </Grid>
+                    </Grid>
+                </MuiPickersUtilsProvider>
+                <div className={classes.button}>
+                    <Button fullWidth variant='contained' color="secondary">Stwórz</Button>
+                </div>
             </div>
         )
     }
