8bddf058ae2cfd31c69dfaa5395c724d79413a04
Adrian Mucha <kestarumper8@gmail.com>
2019-03-01 16:57:29 +0100

    style refactor with airbnb rules

--- frontend-admin/src/components/UploadFileInput.jsx
+++ frontend-admin/src/components/UploadFileInput.jsx
@@ -1,89 +1,98 @@
-import React from 'react'
-import { FormControl, InputLabel, Input, InputAdornment, IconButton, Chip, withStyles } from '@material-ui/core';
-import UploadIcon from '@material-ui/icons/CloudUpload'
+import React from 'react';
+import {
+  FormControl, InputLabel, Input, InputAdornment, IconButton, Chip, withStyles,
+} from '@material-ui/core';
+import UploadIcon from '@material-ui/icons/CloudUpload';
 
 const styles = {
   root: {
     overflow: 'hidden',
-    textOverflow: 'elipsis'
+    textOverflow: 'elipsis',
   },
 
   chip: {
     position: 'absolute',
     bottom: '1px',
-  }
-}
+  },
+};
 
 class UploadFileInput extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      file: null
+      file: null,
     };
-    this.inputRef = React.createRef()
-    this.handleOpenFileSelect = this.handleOpenFileSelect.bind(this)
-    this.handleFileSelected = this.handleFileSelected.bind(this)
-    this.handleFileCancel = this.handleFileCancel.bind(this)
+    this.inputRef = React.createRef();
+    this.handleOpenFileSelect = this.handleOpenFileSelect.bind(this);
+    this.handleFileSelected = this.handleFileSelected.bind(this);
+    this.handleFileCancel = this.handleFileCancel.bind(this);
   }
 
 
-  handleOpenFileSelect(event) {
-    this.inputRef.current.click()
+  handleOpenFileSelect() {
+    this.inputRef.current.click();
   }
 
-  handleFileSelected(event) {
-    const files = this.inputRef.current.files
+  handleFileSelected() {
+    const { files, onChange } = this.inputRef.current;
     if (files.length) {
       this.setState({
-        file: files[0]
-      })
-      this.props.onChange({
+        file: files[0],
+      });
+      onChange({
         target: {
-          value: files[0]
-        }
-      })
+          value: files[0],
+        },
+      });
     }
   }
 
-  handleFileCancel(event) {
-    this.inputRef.current.files = null
-    this.inputRef.current.value = null
-    this.setState({ file: null })
+  handleFileCancel() {
+    this.inputRef.current.files = null;
+    this.inputRef.current.value = null;
+    this.setState({ file: null });
   }
 
   render() {
-    const { classes, label, ...other } = this.props
+    const { classes, label, ...other } = this.props;
+    const { file } = this.state;
     return (
       <FormControl fullWidth className={classes.root}>
-        <InputLabel shrink={!!this.state.file} htmlFor='adornment-fileselect'>{label}</InputLabel>
-        <input ref={this.inputRef} type='file' style={{ visibility: 'hidden' }} onChange={this.handleFileSelected} />
+        <InputLabel shrink={!!file} htmlFor="adornment-fileselect">{label}</InputLabel>
+        <input ref={this.inputRef} type="file" style={{ visibility: 'hidden' }} onChange={this.handleFileSelected} />
         <Input
           {...other}
-          id='adornment-fileselect'
-          type={'text'}
+          id="adornment-fileselect"
+          type="text"
           readOnly
           onChange={this.handleSelectFile}
           onClick={this.handleOpenFileSelect}
           onKeyPressCapture={this.handleOpenFileSelect}
           startAdornment={
-            this.state.file ?
-              <InputAdornment className={classes.chip} position='start'>
-                <Chip onDelete={this.handleFileCancel} label={this.state.file.name} className={classes.chip} />
-              </InputAdornment> : null
+            file
+              ? (
+                <InputAdornment className={classes.chip} position="start">
+                  <Chip
+                    onDelete={this.handleFileCancel}
+                    label={file.name}
+                    className={classes.chip}
+                  />
+                </InputAdornment>
+              ) : null
           }
-          endAdornment={
-            <InputAdornment position='end'>
+          endAdornment={(
+            <InputAdornment position="end">
               <IconButton
-                aria-label='Select file to upload'
+                aria-label="Select file to upload"
                 tabIndex={-1}
               >
                 <UploadIcon />
               </IconButton>
             </InputAdornment>
-          }
+          )}
         />
       </FormControl>
-    )
+    );
   }
 }
 
