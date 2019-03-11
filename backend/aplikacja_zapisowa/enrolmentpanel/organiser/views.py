12725390471bd7ef362fcb509651a73010dab0d6
Sernikjamnika <mati.walczak997@gmail.com>
2019-03-11 22:27:47 +0100

    Add endpoint to get organiser's events and create one

--- backend/aplikacja_zapisowa/enrolmentpanel/organiser/views.py
+++ backend/aplikacja_zapisowa/enrolmentpanel/organiser/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework.response import Response
@@ -9,7 +10,7 @@ from drf_yasg.utils import swagger_auto_schema
 
 from .permissions import IsOrganiserAccount
 from enrolmentpanel.models import Event
-from enrolmentpanel.serializers import StudentSerializer
+from enrolmentpanel.serializers import StudentSerializer, EventSerializer
 
 
 class TestView(APIView):
@@ -33,3 +34,24 @@ class CreateStudentView(APIView):
         if student_serializer.is_valid(raise_exception=True):
             student_serializer.save()
         return Response(student_serializer.data)
+
+
+class CreateEventView(APIView):
+
+    permission_classes = (IsAuthenticated, IsOrganiserAccount)
+    parser_classes = (MultiPartParser,)
+
+    @swagger_auto_schema(request_body=EventSerializer,
+                         operation_description="Creates event")
+    def post(self, request):
+        event_serializer = EventSerializer(data=request.data, context={'user': request.user})
+        if event_serializer.is_valid(raise_exception=True):
+            event_serializer.save()
+        return Response(event_serializer.data)
+
+    @swagger_auto_schema(respoonses={200: EventSerializer},
+                         operation_description="Gets all organisers events")
+    def get(self, request):
+        event = Event.objects.filter(organizer__user=request.user)
+        event_serializer = EventSerializer(event, many=True)
+        return Response(event_serializer.data)
