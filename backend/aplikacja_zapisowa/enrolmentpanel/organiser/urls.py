acacff1a18edacdd198e50a3f26a03d2803a00da
Sernikjamnika <mati.walczak997@gmail.com>
2019-03-11 22:29:02 +0100

    Add Event's endpoint to Organiser's urls

--- backend/aplikacja_zapisowa/enrolmentpanel/organiser/urls.py
+++ backend/aplikacja_zapisowa/enrolmentpanel/organiser/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from .views import (
+    CreateEventView,
     CreateStudentView,
-    TestView
+    TestView,
 )
 
 urlpatterns = [
     path('', TestView.as_view()),
-    path('student', CreateStudentView.as_view())
+    path('student', CreateStudentView.as_view()),
+    path('event', CreateEventView.as_view())
 ]
