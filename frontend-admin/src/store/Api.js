f215824a8280432a5b5cc208400b79622ae6c3d8
Adrian Mucha <kestarumper8@gmail.com>
2019-04-01 10:47:07 +0200

    display events in admin panel

--- frontend-admin/src/store/Api.js
+++ frontend-admin/src/store/Api.js
@@ -1,25 +1,44 @@
 const axios = require('axios');
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8000/api/',
-    timeout: 1000,
+  baseURL: 'http://localhost:8000/api/',
+  timeout: 1000,
 });
 
 export async function authorizeUser(username, password) {
-    try {
-        const token = await instance.post(`/token`, { username, password });
-        return token.data.access;
-    } catch (error) {
-        console.log(`Error: ${error}`);
-    }
+  try {
+    const token = await instance.post('/token/', { username, password });
+    return token.data.access;
+  } catch (error) {
+    console.log(`Error: ${error}`);
+    return null;
+  }
 }
 
 export async function verifyUser(token) {
-    try {
-        const verification = await instance.post(`/token/verify`, { token });
-        const isVerify = verification && verification.status === 200;
-        return isVerify;
-    } catch (error) {
-        return false;
-    }
+  try {
+    const verification = await instance.post('/token/verify/', { token });
+    const isVerify = verification && verification.status === 200;
+    return isVerify;
+  } catch (error) {
+    return false;
+  }
+}
+
+export async function getAllEvents() {
+  const config = {
+    headers: {
+      Authorization: 'Basic dGVzdF9vcmc6dGVzdDEyMzQ=',
+    },
+  };
+  try {
+    const response = await instance.get(
+      '/organiser/event',
+      config,
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
