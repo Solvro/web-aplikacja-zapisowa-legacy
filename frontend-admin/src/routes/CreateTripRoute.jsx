3d39929d21377c9087488f105f284b17cefafc82
Adrian Mucha <kestarumper8@gmail.com>
2019-04-08 23:41:42 +0200

    auto navigate to event list after successful event creation

--- frontend-admin/src/routes/CreateTripRoute.jsx
+++ frontend-admin/src/routes/CreateTripRoute.jsx
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import TripSettingsForm from '../components/TripSettingsForm';
 import { handleCreateTrip } from '../store/Api';
 
-export default class CreateTripRoute extends Component {
+class CreateTripRoute extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
   render() {
+    const { history } = this.props;
     return (
-      <TripSettingsForm {...this.props} onSubmit={handleCreateTrip} />
+      <TripSettingsForm
+        {...this.props}
+        onSubmit={(data) => {
+          handleCreateTrip(data).then((statusOk) => {
+            if (statusOk) {
+              history.push('/trips');
+            } else {
+              alert('ERROR WHILE CREATING EVENT');
+            }
+          });
+        }
+      }
+      />
     );
   }
 }
+
+export default withRouter(CreateTripRoute);
