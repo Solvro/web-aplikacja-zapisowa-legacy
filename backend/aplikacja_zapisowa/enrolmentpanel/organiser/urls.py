533c1536b42af4c7b29087cbbf729cdf4f14d105
Mateusz Walczak <mati.walczak997@gmail.com>
2019-04-11 22:04:49 +0000

    Resolve "Display Room Tables"

--- backend/aplikacja_zapisowa/enrolmentpanel/organiser/urls.py
+++ backend/aplikacja_zapisowa/enrolmentpanel/organiser/urls.py
@@ -4,12 +4,14 @@ from .views import (
     CreateEventView,
     CreateStudentView,
     TestView,
-    DetailEventView
+    DetailEventView,
+    DetailRoomListView
 )
 
 urlpatterns = [
     path('event', CreateEventView.as_view()),
     path('event/<event_name>', DetailEventView.as_view()),
+    path('event/<event_name>/rooms', DetailRoomListView.as_view()),
     path('<event_name>/test/', TestView.as_view()),
     path('<event_name>/student/', CreateStudentView.as_view())
 ]
