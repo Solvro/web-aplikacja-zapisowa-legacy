9f6c337a9c99fca3901968f70a521c10eed5303c
okkindel <ebrithil97@gmail.com>
2019-04-25 22:41:15 +0200

    fix(homepage): styles overflow

--- frontend-user/src/components/UserFormRoute/UserFormRouteStyles.ts
+++ frontend-user/src/components/UserFormRoute/UserFormRouteStyles.ts
@@ -2,6 +2,7 @@ import {createStyles, Theme} from "@material-ui/core";
 
 export const userFormRouteStyles = (theme: Theme) => createStyles({
     mainContainer: {
+        height: '100%',
         display: 'flex',
         flex: '1 1 auto',
         alignItems: 'center',
