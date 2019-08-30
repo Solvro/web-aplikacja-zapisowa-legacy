23752ab3669d0ea54ace2a25fb68562d0b9ba2eb
Sernikjamnika <mati.walczak997@gmail.com>
2019-08-30 10:35:37 +0200

    Add tests for ListCreateEventViewTestCase

--- backend/aplikacja_zapisowa/enrolmentpanel/tests.py
+++ backend/aplikacja_zapisowa/enrolmentpanel/tests.py
@@ -9,11 +9,11 @@ from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 from rest_framework import status
 
-from .serializers import StudentSerializer
+from .serializers import EventSerializer
 
+import json
 import datetime
 import logging
-import json
 
 # class AdminTestCase(TestCase):
 #     username = 'sernikjamnika'
@@ -90,75 +90,6 @@ import json
 #                            sex='F')
 #         self.assertRaises(IntegrityError, student2.save)
 
-class CreateStudentViewTestCase(TestCase):
-    EVENT_NAME = 'test_event'
-    STUDENT_DATA = {
-        'name': 'test_student',
-        'index': '2137',
-        'faculty': 1,
-        'sex': 'M'
-    }
-    ORGANISER_DATA = {
-        'username': 'test_organiser',
-        'password': 'test',
-        'faculty': 1,
-        'name': 'test'
-    }
-    url = reverse('student-create', kwargs={'event_name': EVENT_NAME})
-    client = APIClient()
-
-    def setUp(self):
-        organiser = Organiser.objects.create(**self.ORGANISER_DATA)
-        Event.objects.create(
-            name=self.EVENT_NAME,
-            beginning_date=datetime.datetime.now(),
-            ending_date=datetime.datetime.now(),
-            organizer=organiser
-        )
-
-    def test_event_owner_permission(self):
-        Organiser.objects.create(
-            username='other_test_organiser',
-            password='test',
-            faculty=1,
-            name='test'
-        )
-        self.client.login(username='other_test_organiser', password='test')
-        response = self.client.post(
-            self.url,
-            json.dumps(self.STUDENT_DATA),
-            content_type='application/json'
-        )
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    def test_create_student(self):
-        self.client.login(username=self.ORGANISER_DATA['username'], password=self.ORGANISER_DATA['password'])
-        response = self.client.post(
-            self.url,
-            json.dumps(self.STUDENT_DATA),
-            content_type='application/json'
-        )
-        test_student = Student.objects.filter(name='test_student')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(len(test_student), 1)
-        self.assertEqual(response.data, StudentSerializer(test_student[0]).data)
-
-    def test_double_creation(self):
-        self.client.login(username=self.ORGANISER_DATA['username'], password=self.ORGANISER_DATA['password'])
-        self.client.post(
-            self.url,
-            json.dumps(self.STUDENT_DATA),
-            content_type='application/json'
-        )
-        response = self.client.post(
-            self.url,
-            json.dumps(self.STUDENT_DATA),
-            content_type='application/json'
-        )
-        test_student = Student.objects.filter(name='test_student')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(len(test_student), 1)
-
 
 # class EventTestCase(TestCase):
 
@@ -217,7 +148,6 @@ class CreateStudentViewTestCase(TestCase):
 #     #     self.assertEqual(20, len(Student.objects.all()))
 #     #     self.assertEqual(20, len(Room.objects.all()))
 
-#
 #
 # class RoomTestCase(TestCase):
 #     def setUp(self):
@@ -279,3 +209,109 @@ class CreateStudentViewTestCase(TestCase):
 #         room.save()
 #         room.add_people(people)
 #         self.assertNotIn(room, [person.room for person in people])
+
+
+class ListCreateEventViewTestCase(TestCase):
+    ORGANISER_DATA = {
+        'username': 'test_organiser',
+        'password': 'test',
+        'faculty': 1,
+        'name': 'test'
+    }
+    EVENT_DATA = {
+        'name': 'test_event',
+        'beginning_date': '2019-12-21',
+        'ending_date': '2019-12-22',
+        'description': 'this is test event',
+        'participants': open('./files_for_testing/generated_participants.csv'),
+        'rooms': open('./files_for_testing/generated_rooms.csv')
+    }
+    CORRUPTED_ROOM_DATA = './files_for_testing/corrupted_rooms.csv'
+    
+    url = reverse('event-create-list')
+    client = APIClient()
+
+    def setUp(self):
+        self.organiser = Organiser.objects.create(**self.ORGANISER_DATA)
+        self.EVENT_DATA['organizer'] = self.organiser
+        self.EVENT_DATA['participants'].seek(0)
+        self.EVENT_DATA['rooms'].seek(0)
+        self.client.login(
+            username=self.ORGANISER_DATA['username'],
+            password=self.ORGANISER_DATA['password']
+        )
+
+    def test_event_creation(self):
+        response = self.client.post(
+            self.url,
+            self.EVENT_DATA
+        )
+
+
+        event = Event.objects.get(name=self.EVENT_DATA['name'])
+        students = Student.objects.filter(event=event)
+        rooms = Room.objects.filter(event=event)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, EventSerializer(event).data)
+
+        # check if rooms and students were created
+        self.assertNotEqual(len(students), 0)
+        self.assertNotEqual(len(rooms), 0)
+
+        # check if newly created event is not active
+        self.assertFalse(response.data['is_active'])
+
+    def test_atomic_event_creation(self):
+        changed_event_data = self.EVENT_DATA.copy()
+        changed_event_data['rooms'] = open(self.CORRUPTED_ROOM_DATA)
+
+        response = self.client.post(
+            self.url,
+            changed_event_data
+        )
+
+        events = Event.objects.all()
+        rooms = Room.objects.all()
+        students = Student.objects.all()
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(len(events), 0)
+        self.assertEqual(len(rooms), 0)
+        self.assertEqual(len(students), 0)
+
+    def test_list_of_events_getting(self):
+        event_data_copy = self.EVENT_DATA.copy()
+        event_data_copy.pop('participants')
+        event_data_copy.pop('rooms')
+
+        Event.objects.create(**event_data_copy)
+        event_data_copy['name'] += '_1'
+        Event.objects.create(**event_data_copy)
+
+        response = self.client.get(self.url)
+
+        events = Event.objects.filter(organizer=self.organiser)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, EventSerializer(events, many=True).data)
+
+    def test_others_event_filtering(self):
+        organiser_data_copy = self.ORGANISER_DATA.copy()
+        organiser_data_copy['username'] += '_1'
+        other_organiser = Organiser.objects.create(**organiser_data_copy)
+
+        event_data_copy = self.EVENT_DATA.copy()
+        event_data_copy.pop('participants')
+        event_data_copy.pop('rooms')
+        event_data_copy['organizer'] = other_organiser
+        Event.objects.create(**event_data_copy)
+
+        response = self.client.get(self.url)
+        
+        # other event is not displayed because it is other organizer's event
+        events = Event.objects.filter(organizer=self.organiser)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, EventSerializer(events, many=True).data)
+
+        all_events = Event.objects.all()
+        self.assertNotEqual(response.data, EventSerializer(all_events, many=True).data)
